ed6509d31aa37b99d460cdbbb16c4c8e905d8b7d
Noah Hendrickson <noah@noahhendrickson.com>
2025-04-08 19:51:46 -0400

    Added Jackson as an api of warcoat-common and also setup publishing with GitHub packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("java-library")
+    id("maven-publish")
 }
 
 group = "com.noahhendrickson"
@@ -10,10 +12,37 @@ repositories {
 }
 
 dependencies {
+    api("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    api("com.fasterxml.jackson.core:jackson-core:2.18.3")
+    api("com.fasterxml.jackson.core:jackson-annotations:2.18.3")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/NoahH99/warcoat-common")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
